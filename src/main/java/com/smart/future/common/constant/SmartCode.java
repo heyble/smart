a7e4c66536ade5cac18736f0edf45f2ed4a11a55
chandler <596202898@qq.com>
2020-03-29 23:31:41 +0800

    Complete the Storage module, support large files, and resume uploading with breakpoints.

--- src/main/java/com/smart/future/common/constant/SmartCode.java
+++ src/main/java/com/smart/future/common/constant/SmartCode.java
@@ -3,6 +3,7 @@ package com.smart.future.common.constant;
 public interface SmartCode {
     Integer OK = 200;
 
+
     interface LoginError{
         Integer AUTH_ERROR = 1000;
     }
@@ -10,5 +11,13 @@ public interface SmartCode {
     interface UserError{
         Integer EMPTY_PARAM = 2010;
         Integer ERROR_PARAM = 2011;
+    }
+
+    interface Storage{
+        Integer EMPTY_PARAM = 3010;
+        Integer ERROR_PARAM = 3011;
+        Integer REQUEST_ERROR = 3020;
+        Integer INTERNAL_ERROR = 500;
+        Integer NOT_FOUND = 4010;
     }
 }
